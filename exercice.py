1d6ff1a8b14525b0708ae1d753b23fc7622a84f1
hubbou <hubert.boucher@live.ca>
2020-09-14 17:53:01 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def orthogonal(vec1, vec2):
     return (vec1[0] * vec2[0] + vec1[1] * vec2[1]) == 0
 
 def average(list):
-    return (vec1[0] * vec2[0] + vec1[1] * vec2[1]) == 0
+    for nb
 
 
 if __name__ == '__main__':
