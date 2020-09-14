67a3dfc67c56a73f5de12d7380b8ac1e6066e260
hubbou <hubert.boucher@live.ca>
2020-09-14 17:56:17 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,6 +24,9 @@ def average(list):
         
     return somme / count
 
+def orthogonal(vec1, vec2):
+    return (vec1[0] * vec2[0] + vec1[1] * vec2[1]) == 0
+
 
 if __name__ == '__main__':
     mots = [
